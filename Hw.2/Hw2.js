863865aad5e59dae784916852c55f75c083316ad
fares <faresxalamri2@gmail.com>
2023-08-10 15:05:45 +0300

    hw-2

---
+++ Hw.2/Hw2.js
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////
+
+let num_1=42;
+let NumTostr=String(num_1);
+
+console.log(`${typeof NumTostr}:${NumTostr}`);
+
+////////////////////////////////////////////////
+
+let str_1="25"
+let StrToNum=Number(str_1);
+console.log(`${typeof StrToNum}:${StrToNum}`);
+
+////////////////////////////////////////////////
+
+let str_2="10";
+let StrToNum_2=Number(str_2);
+
+console.log(`${str_2 - 5}`);
+console.log(`${str_2 + 5}`);
+
+/////////////////////////////////////////////////
+
+let str_3="15.5";
+let StrToFloat=Number(str_3);
+
+console.log(`${str_3 * 2}`);
+console.log(`${str_3 / 2}`);
+
+//////////////////////////////////////////////////
+
+let str_4="5";
+let num_2=3;
+
+console.log(`${str_4 + num_2}`);
+
+//////////////////////////////////////////////////
+
+let bool=true;
+let num_3=1;
+
+console.log(`${bool + num_3}`);
+
+///////////////////////////////////////////////////
