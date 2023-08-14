62899b94b30e0996b90bf2e01a100d2ec46382cd
fares <faresxalamri2@gmail.com>
2023-08-14 16:19:59 +0300

    Hw-3

---
+++ Hw.3/Hw.3.js
@@ -0,0 +1,9 @@
+let names=["Fares","Ahmed","Saleh","sultan","Al-amri"]
+
+console.log(names.slice(2));
+
+console.log(names.unshift("Ali"));
+console.log(names.push("Samah"));
+
+console.log(names.shift());
+console.log(names.pop());
