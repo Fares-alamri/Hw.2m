43ce6c85fe70488504eda18d26a9f05a66927c8d
fares <faresxalamri2@gmail.com>
2023-08-16 01:03:59 +0300

    Hw.4

---
+++ Hw.4/Hw.4.js
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////
+
+//let q1 = prompt("Enter you name");
+//let q2 = prompt("Enter your age");
+
+//if (q2 >= 18) {
+//   console.log("Welcome " + q1);
+//}else{
+//    console.log(" sorry " +q1+ " you cant drive ");
+//}
+
+///////////////////////////////////////////////////////////////
+
+let name_1 = prompt("Enter you name");
+
+if (name_1== "Ahmed"){
+    console.log("الاسمان  متطابقان");
+}else{
+    console.log("الاسمان غير متطابقان");
+}
