85c440e6765981aabd1bca865d2fa22c13ef6795
fares <faresxalamri2@gmail.com>
2023-08-21 15:01:01 +0300

    Hw.6

---
+++ Hw.6/Hw.6.js
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////////////////
+
+//let javascript_Boot_camp = (teach) => {
+
+//    teach=prompt(" Enter teacher number : ")
+
+//    let Teacher1 = "Samah"
+//    let Teacher2 = "Ali"
+
+//    if (teach == 1) {
+//        alert(" This javaScript BootCamp with teacher " + Teacher1);
+//    }else if (teach == 2) {
+//        alert(" This javaScript BootCamp with teacher " + Teacher2);
+//    }else{
+//        alert("Not available");
+//    }
+
+//}
+
+//javascript_Boot_camp(1)
+
+////////////////////////////////////////////////////////////////////////////
+
+let calculateAverage = (num1, num2) =>{
+    let average = (num1 + num2) / 2;
+    return average;
+  }
+  
+// integer
+  let integer = calculateAverage(5, 10);
+  alert(" Average for integer numbers : " + integer)
+  
+// double
+  let double = calculateAverage(2.5, 7.8);
+  alert(" Average for double numbers :  " + double)
+
+/////////////////////////////////////////////////////////////////////////////
